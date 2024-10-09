61e40dc8fd7e1c446886c276876bef0f486978c6
Aarush <kraarush709@gmail.com>
2024-10-10 03:19:46 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './Components/Header';
+import List from './Components/List';
+import Footer from './Components/Footer';
+import './App.css'
+
+function App(){
+  return(
+    <div>
+      <Header />
+      <List /> 
+      <Footer />
+    </div>
+  )
+}
+
+export default App
