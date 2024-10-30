0544353be36a61263bb02cab06f397bd45b5ea9c
Aarush <kraarush709@gmail.com>
2024-10-31 00:32:46 +0530

    added dummy notes using map and arrow function from a seperate notes.js file

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from 'react'
 import Header from './Components/Header';
 import List from './Components/List';
 import Footer from './Components/Footer';
-import './App.css'
+import notes from './notes';
 
 function App(){
   return(
     <div>
       <Header />
-      <List /> 
+      {notes.map((eachItem) => (
+        <List 
+          key = {eachItem.key}
+          title = {eachItem.title}
+          content = {eachItem.content}
+        />
+      ))} 
       <Footer />
     </div>
   )
