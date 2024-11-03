7a916c74a08498c64e338438d78839206aee94b2
Aarush <kraarush709@gmail.com>
2024-11-03 17:50:11 +0530

    Made new changes in the build of the project

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-function Header(){
-    return (
-        <div>
-            <header>
-                <h1>Keeper</h1>
-            </header>
-        </div>
-    );
+function Header() {
+  return (
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  );
 }
 
 export default Header;
