e111d2304116626907aac1d8eb95778741eb2b62
Aarush <kraarush709@gmail.com>
2024-11-05 02:30:57 +0530

    Preparing backend for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,19 +1,32 @@
 import express from "express";
 import cors from "cors";
-import dummyData from "./data.js";
+import pg from "pg";
+import env from 'dotenv';
 
 const port = 5000;
 const app = express();
+const { Pool } = pg;
+env.config();
 app.use(cors());
 
-app.get("/getData", (req, res) => {
-  try {
-    res.send(dummyData);
-  } catch(err) {
-    console.log(err);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL, 
+  ssl: {
+    rejectUnauthorized: false 
   }
 });
 
+app.get("/getData", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM dummy"); 
+    res.send(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error retrieving data from database");
+  }
+});
+
+
 app.listen(port, () => {
-    console.log("Server is live on " + port);
+  console.log("Server is live on " + port);
 });
